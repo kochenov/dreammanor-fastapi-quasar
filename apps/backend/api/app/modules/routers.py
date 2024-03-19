48dc20b4e259d21357852823078ec30532b4c632
kochenov <kochenof@yandex.ru>
2024-03-19 09:29:31 +0300

    Initial commit

---
+++ apps/backend/api/app/modules/routers.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+# from app.modules.links.router import router as router_links
+# from app.modules.parsing_process.router import router as router_process
+
+api_router = APIRouter()
+
+# api_router.include_router(router_links, tags=["links"])
+# api_router.include_router(router_process, tags=["parsing_process"])
